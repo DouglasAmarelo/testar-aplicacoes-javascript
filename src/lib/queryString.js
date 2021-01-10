84809119e5cd7898202349ac93933b12f38bd791
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2021-01-10 11:39:54 -0300

    query string method and tests

---
+++ src/lib/queryString.js
@@ -0,0 +1,36 @@
+const keyValueToString = ([key, value]) => {
+  const typeOfValue = Object.prototype.toString.call(value);
+
+  if (typeOfValue === '[object Object]') {
+    throw new Error('Please check your parameters');
+  }
+
+  return `${key}=${value}`;
+};
+
+const queryString = obj => {
+  const result = Object.entries(obj).map(keyValueToString).join('&');
+
+  return result;
+};
+
+const parse = str => {
+  const result = Object.fromEntries(
+    str.split('&').map(item => {
+      let [key, value] = item.split('=');
+
+      if (value.includes(',')) {
+        value = value.split(',');
+      }
+
+      return [key, value];
+    })
+  );
+
+  return result;
+};
+
+module.exports = {
+  queryString,
+  parse,
+};
